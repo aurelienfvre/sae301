9a3b28cda429c1db70433ef883d6ef9adffb5ffa
aurelienfvre <aurelieuxpro@gmail.com>
2023-12-03 17:41:04 +0100

    finalisation et ajout du calendrier d'évènements

--- src/Controller/CalendarController.php
+++ src/Controller/CalendarController.php
@@ -5,15 +5,115 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 class CalendarController extends AbstractController
 {
     #[Route('/calendar', name: 'app_calendar')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
+        $session = $request->getSession();
+        $username = $session->get('user')['prenom'] ?? null;
+
+        // Chemin vers le fichier cards.json
+        $path = $this->getParameter('kernel.project_dir') . '/var/data/cards.json';
+        $cards = file_exists($path) ? json_decode(file_get_contents($path), true) : [];
+        $cardsJson = json_encode($cards);
+
+        $group_items = [
+            ['value' => 'A', 'label' => 'A'],
+            ['value' => 'B', 'label' => 'B'],
+            ['value' => 'C', 'label' => 'C'],
+            ['value' => 'D', 'label' => 'D'],
+            ['value' => 'E', 'label' => 'E'],
+            ['value' => 'F', 'label' => 'F'],
+            ['value' => 'G', 'label' => 'G'],
+            ['value' => 'H', 'label' => 'H'],
+
+        ];
+        $rankItems = [
+            ['value' => 'S1', 'label' => 'S1'],
+            ['value' => 'S2', 'label' => 'S2'],
+            ['value' => 'S3', 'label' => 'S3'],
+            ['value' => 'S4', 'label' => 'S4'],
+            ['value' => 'S5', 'label' => 'S5'],
+            ['value' => 'S6', 'label' => 'S6'],
+        ];
+
+        $cardColors = [
+            'yellow' => '#FADB39',
+            'blue' => '#007BFF',
+            // Ajoutez d'autres couleurs pour d'autres classes ici
+        ];
+
+        // Ajoutez d'autres tableaux de couleurs ici
+        $cardBorderColors = [
+            'yellow' => '#FADB39',
+            'blue' => '#007BFF',
+        ];
+
+        $cardDateBackgroundColors = [
+            'yellow' => '#',
+            'blue' => '#...',
+        ];
+
+        $cardMatiereBorderColors = [
+            'yellow' => '#EAC917',
+            'blue' => '#1264DC',
+        ];
+
+        $cardDetailsColors = [
+            'yellow' => '#...',
+            'blue' => 'white',
+        ];
+
+        $cardSendColors = [
+            'yellow' => '#...',
+            'blue' => 'white',
+        ];
+
+        $cardEmailAddressColors = [
+            'yellow' => '#...',
+            'blue' => 'white',
+        ];
+        $cardTitleColors = [
+            'yellow' => '#', // exemple de couleur pour le titre
+            'blue' => '#fff',
+            // Autres couleurs pour d'autres classes
+        ];
+        $cardDetailItemColors = [
+            'yellow' => '#', // exemple de couleur pour les éléments de détail
+            'blue' => '#fff',
+            // Autres couleurs pour d'autres classes
+        ];
+
+        // Données pour les dropdowns
+
+
+
         return $this->render('calendar/index.html.twig', [
             'controller_name' => 'CalendarController',
             'current_page' => 'calendar',
+            'cards' => $cards,
+            'username' => $username,
+            'card_colors' => $cardColors,
+            'card_border_colors' => $cardBorderColors,
+            'card_date_background_colors' => $cardDateBackgroundColors,
+            'card_matiere_border_colors' => $cardMatiereBorderColors,
+            'card_details_colors' => $cardDetailsColors,
+            'card_send_colors' => $cardSendColors,
+            'card_email_address_colors' => $cardEmailAddressColors,
+            'cardsJson' => $cardsJson,
+            'card_rank_items' => $rankItems,
+            'group_items' => $group_items,
+            'card_title_colors' => $cardTitleColors,
+            'card_detail_item_colors' => $cardDetailItemColors,
+            'date' => '28 Novembre 2023',
+
         ]);
     }
+
+
 }
